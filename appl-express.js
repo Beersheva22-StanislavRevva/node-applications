e86def09f62b6e5a8e8fc992a9c00883bffa8662
stanislav-revva <nivadrive1@gmail.com>
2023-09-22 10:39:29 +0300

    commit

--- appl-express.js
+++ appl-express.js
@@ -1,21 +1,34 @@
+import bodyParser from 'body-parser';
 import express from 'express'
 import { users } from './routes/users.mjs';
 import cors from 'cors';
-import bodyParser from 'body-parser'
 import morgan from 'morgan';
-import errorHandler from './middleware/errorHandler.mjs';
 import config from 'config';
+import errorHandler from './middleware/errorHandler.mjs';
 import auth from './middleware/auth.mjs';
-import  {employees} from './routes/employees.mjs'
+import { employees } from './routes/employees.mjs';
+import expressWs from 'express-ws';
 
 const app = express();
+const expressWsInstant = expressWs(app);
+const wss = expressWsInstant.getWss();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(auth);
-app.use('/employees', employees)
+app.ws('/employees/websocket', (ws, req) => {
+    console.log(`connection from ${req.socket.remoteAddress}`)
+    ws.send("Hello");
+    wss.clients.forEach(socket => socket.send(`number of connections is ${wss.clients.size}, protocol ${ws.protocol}`))
+})
+app.use((req,res,next) => {
+    req.wss = wss;
+    next();
+})
+app.use('/employees', employees);
 app.use('/users',users);
-const port = process.env.PORT || config.get('server.port');
+const port = process.env.PORT || config.get('server.port')
 const server = app.listen(port);
-server.on("listening", () => console.log(`server is listening on port ${server.address().port}`));
+server.on("listening", () => console.log(`server is listening on port ${server.address().port}`))
 app.use(errorHandler);
